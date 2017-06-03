5dfbc501e45419f33eba14f6da38d96cb26b5750
vagrant <vagrant@vagrant.vm>
2017-06-03 13:46:47 +0000

    boilerplate

---
+++ src/main/webapp/app/shared/shared.module.ts
@@ -0,0 +1,56 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import { CookieService } from 'angular2-cookie/services/cookies.service';
+import {
+    JhipsterAngular2SharedLibsModule,
+    JhipsterAngular2SharedCommonModule,
+    CSRFService,
+    AuthServerProvider,
+    AccountService,
+    UserService,
+    StateStorageService,
+    LoginService,
+    LoginModalService,
+    Principal,
+    HasAnyAuthorityDirective,
+    JhiSocialComponent,
+    SocialService,
+    JhiLoginModalComponent
+} from './';
+
+@NgModule({
+    imports: [
+        JhipsterAngular2SharedLibsModule,
+        JhipsterAngular2SharedCommonModule
+    ],
+    declarations: [
+        JhiSocialComponent,
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective
+    ],
+    providers: [
+        CookieService,
+        LoginService,
+        LoginModalService,
+        AccountService,
+        StateStorageService,
+        Principal,
+        CSRFService,
+        AuthServerProvider,
+        SocialService,
+        UserService,
+        DatePipe
+    ],
+    entryComponents: [JhiLoginModalComponent],
+    exports: [
+        JhipsterAngular2SharedCommonModule,
+        JhiSocialComponent,
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective,
+        DatePipe
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
+})
+export class JhipsterAngular2SharedModule {}
